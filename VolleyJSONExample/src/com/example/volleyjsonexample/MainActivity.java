7ceee3bdec5abd3b977721512abcc15069e880c5
Guilherme Mota <guimota93@hotmail.com>
2014-05-07 11:53:21 +0100

    Quarta 7 Maio 2014 - VolleyExample 2 add

--- VolleyJSONExample/src/com/example/volleyjsonexample/MainActivity.java
+++ VolleyJSONExample/src/com/example/volleyjsonexample/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.volleyjsonexample;
 
 import java.util.ArrayList;
 
-import android.app.DownloadManager.Request;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -17,21 +24,46 @@ public class MainActivity extends ActionBarActivity {
 
 	ListView videoList;
 	ArrayList<String> videoArray = new ArrayList<String>();
-	
+	String feedUrl = "https://gdata.youtube.com/feeds/api/users/twistedequations/uploads?v2&alt=json&start-index=1&max-results=30";
+	Context context;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		context = this;
 		setContentView(R.layout.activity_main);
 		videoList = (ListView) findViewById(R.id.videoList);
 		
-		ArrayAdapter <String> videoAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, videoArray);
+		final ArrayAdapter <String> videoAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, videoArray);
 		videoList.setAdapter(videoAdapter);
+		videoAdapter.add("1");
 		
 		RequestQueue rq = Volley.newRequestQueue(this);
-		
-//		JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Me, null, null, null, null) {
-//		};
-		
+		JsonObjectRequest jsonRequest = new JsonObjectRequest(com.android.volley.Request.Method.GET, feedUrl, null, 
+				new Response.Listener<JSONObject>() {
+
+					@Override
+					public void onResponse(JSONObject response) {
+						try {
+							JSONArray videos = response.getJSONObject("data").getJSONArray("items");
+							for (int i = 0; i < videos.length(); i++) {
+								videoArray.add(videos.getJSONObject(i).getString("title"));
+							}
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						videoAdapter.notifyDataSetChanged();
+					}
+		}, new Response.ErrorListener() {
+
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+		rq.add(jsonRequest);
 	}
 
 	@Override
@@ -42,6 +74,4 @@ public class MainActivity extends ActionBarActivity {
 		return true;
 	}
 
-	
-	
 }
